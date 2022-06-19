b4e861baa1a2a165bb6b47c12c87efa7ea954c1f
Logan Someone <loganqqq@outlook.com>
2022-06-19 13:27:12 -0400

    remove a unneeded import

--- src/main/java/me/starstriker/antichatreportingplugin/AntiChatReportingPlugin.java
+++ src/main/java/me/starstriker/antichatreportingplugin/AntiChatReportingPlugin.java
@@ -1,6 +1,5 @@
 package me.starstriker.antichatreportingplugin;
 
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import me.starstriker.antichatreportingplugin.events.*;
 
