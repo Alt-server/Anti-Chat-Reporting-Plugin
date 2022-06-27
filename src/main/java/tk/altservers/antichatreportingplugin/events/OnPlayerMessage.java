953b58b31e1e708a81079fe9d5097b6cd8fbdc97
Logan Someone <loganqqq@outlook.com>
2022-06-27 13:59:05 -0400

    e

--- src/main/java/tk/altservers/antichatreportingplugin/events/OnPlayerMessage.java
+++ src/main/java/tk/altservers/antichatreportingplugin/events/OnPlayerMessage.java
@@ -3,17 +3,14 @@ package tk.altservers.antichatreportingplugin.events;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChatEvent;
-import org.bukkit.plugin.java.JavaPlugin;
-
+import org.bukkit.plugin.java.JavaPlugin; 
 import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.FileConfiguration;
 
 public class OnPlayerMessage implements Listener {
     @EventHandler
     public void onPlayerChat(PlayerChatEvent event, JavaPlugin plugin) 
     {
       // stop the server from sending the message
-      // get the config and store it in a variable
       event.setCancelled(true);
       String msg = event.getMessage();
       String p = event.getPlayer().getDisplayName();
